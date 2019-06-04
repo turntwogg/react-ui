e010c4904b4ea38a71370115f75d559f963ffd13
Drew Bolles <drewbolles@gmail.com>
2019-06-04 05:19:25 -0400

    Adjust default exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-export { default as Container } from './Container';
 export { default as Row } from './Row';
 export { default as Col } from './Col';
 export { default as ThemeProvider } from './ThemeProvider';
-export { default as Visibility } from './Visibility';
 export { default as useTheme } from './useTheme';
 export { default as theme } from './theme';
